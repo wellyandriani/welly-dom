79481af6e8ee795077af7e48b0a3a4ea215e992d
wellyandriani <wellyandriani97@gmail.com>
2018-07-13 16:34:50 +0700

    add address box

---
+++ domwelly/address-book/index.js
@@ -0,0 +1,29 @@
+console.log("Manipulation String");
+
+const title = document.getElementById("title")
+const output = document.getElementById("output-string")
+const result = document.getElementById("button-btn")
+
+const add = () => {
+    const firstName = document.getElementById("firstname").value
+    const lastName = document.getElementById("lastname").value
+    const Birthday = document.getElementById("birthday").value
+    const Address = document.getElementById("address").value
+    const Email = document.getElementById("email").value
+    const Phone = document.getElementById("phone").value
+
+    console.log("Name : ", firstName, lastName);
+    console.log("Date of Birthday: ", Birthday);
+    console.log("Address: ", Address);
+    console.log("Email: ", Email);
+    console.log("Phone: ", Phone);
+
+    output.innerText = `Name : ${firstName} ${lastName}
+    Date of Birthday : ${Birthday}
+    Address : ${Address}
+    Email : ${Email} 
+    Phone : ${Phone}`
+
+}
+
+result.addEventListener('click', add);
--- domwelly/manipulation-string/index.js
+++ domwelly/manipulation-string/index.js
@@ -2,21 +2,28 @@ console.log("Manipulation String");
 
 const title = document.getElementById("title")
 const output = document.getElementById("output-string")
+const result = document.getElementById("button-btn")
 
 const add = () => {
     const firstName = document.getElementById("firstname").value
     const lastName = document.getElementById("lastname").value
     const Birthday = document.getElementById("birthday").value
-    const Address = document.getElementById("firstname").value
+    const Address = document.getElementById("address").value
+    const Email = document.getElementById("email").value
+    const Phone = document.getElementById("phone").value
 
     console.log("Name : ", firstName, lastName);
     console.log("Date of Birthday: ", Birthday);
     console.log("Address: ", Address);
+    console.log("Email: ", Email);
+    console.log("Phone: ", Phone);
 
     output.innerText = `Name : ${firstName} ${lastName}
-    Date of Birthaday : ${Birthday}
-    Address : ${Address}`
+    Date of Birthday : ${Birthday}
+    Address : ${Address}
+    Email : ${Email} 
+    Phone : ${Phone}`
 
 }
 
-output.addEventListener('click',add);
+result.addEventListener('click', add);
